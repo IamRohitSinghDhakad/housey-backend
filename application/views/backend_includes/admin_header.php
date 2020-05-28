22e2f5304dcbfac80d34b48e83862a90e7276c48
jyoti <jyoti.mindiii@gmail.com>
2020-05-28 12:27:25 +0530

    Admin fixes

--- application/views/backend_includes/admin_header.php
+++ application/views/backend_includes/admin_header.php
@@ -51,9 +51,9 @@
     <!-- Logo -->
     <a href="" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><img src="<?php echo base_url().HOUSEY_LOGO; ?>logo.png"></span>
+      <span class="logo-mini"><img src="<?php echo base_url().HOUSEY_LOGO; ?>logo.png" height ="60" ></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><img src="<?php echo base_url().HOUSEY_LOGO;?>logo.png" id="logo-image"></span>
+      <span class="logo-lg"><img src="<?php echo base_url().HOUSEY_LOGO;?>logo.png" height ="60" id=""></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
